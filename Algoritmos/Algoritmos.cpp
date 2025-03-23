0b250c47c4ac997f4444ff6161a2af5d35844c44
Pedro S. Teixeira <pedrotex00santos@gmail.com>
2025-03-23 12:41:33 -0300

    Finaliza o slide 3

--- Algoritmos/Algoritmos.cpp
+++ Algoritmos/Algoritmos.cpp
@@ -41,15 +41,174 @@ void fun2_Slide1(){
     cout << "Tempo 2 (ms): " << ((clock()-t0)*1.0/CLOCKS_PER_SEC)*1000 << endl;
 }
 
-/* T(n) = Top . C(n)
-   C(n) = 0 + 1 + 2 + 3 + 4 + ... = somatorio i de 0 ate n-1 = n(n-1)/2
-    0.1 * [((n-1) * n)/2]
+/* T(n) = Top * C(n)
+   C(n) = 0 + 1 + 2 + 3 + 4 + ... = somatorio i de 0 ate n-1 = n(n-1)/2 = (n^2 - n)/2 = n^2 para valores grandes
+    T.op* [((n-1) * n)/2]
 */
 void fun3_Slide1(int *n){
     int soma = 0;
-    for(int i=1;i<=*n;i++)
+
+    for(int i=1;i<=*n;i++)          // para os dois for = n(n-1)/2 
 	    for(int j=0;j<i;j++)
-		    soma++;
+		    soma++;                 // ??? o tempo depende do compilador
+}
+
+// O(n-1) = O(n)
+int maxElement(int v[],int n) {     //fun1_slide2
+	int mx = v[0];
+	for(int i=1;i<n;i++)        //executa n-1 vezes
+		if (v[i] > mx)          //execua n-1 vezes -> op. basica comparacao
+			mx = v[i];          //execua somente quando v[i] > mx
+	return mx;
+}
+
+//O(n)
+// pior caso => a chave nao esta no vetor ou a chave esta no ultimo elemento => O(n)
+// caso medio => a chave esta no vetor, nem no primeiro nem no ultimo => O(n)
+// caso otimo => a chave esta no primeiro elemento do vetor, 1 interacao => O(1)
+int find(int v[],int n,int elem) {  //fun2_slide2
+	for(int i=0;i<n;i++)            //Executa n vezes O(n)
+		if (v[i] == elem)           //Executa n vezes -> op. basica comparacao
+			return i;
+	return -1; 
+}
+
+// o nivel de complexidade de um while depende da sua condicao de parada tambem
+// a funcao calcula o numero de digitos, o contador aumenta para cada divisao inteira por 10, quando 0<n<1 ele para
+// O (log(n) + 1) = O (log (n))
+int numDigitos(int n) { //suponha que n>0       //fun3_slide2
+	int ct  = 0;
+	while( n != 0) {                         
+		n/=10;
+		ct++;          //O(1)
+	}
+	return ct;
+}
+
+//tam entrada = v[0] pois n eh irrelevante para o algoritmo e so eh utilizado o primeiro elemento do vetor
+//op.basica = divisao
+//O(log(v[0]))
+int numDigitosPrimElemento(int v[], int n) {  ///func4_slide_2
+	int x = v[0];       //unico elemento utilizado dos parametros              
+	int ct  = 0;
+
+	while( x != 0) {   //executa v[0]/10 != 0 vezes 
+		x/=10;          //op.basica
+		ct++;
+	}
+	return ct;
+}
+
+//O(n^3)
+int matrixMult(int **a, int **b, int **c, int n) { //suponha que n>0  ///func5_slide_2
+	for(int i=0;i<n;i++)                //O(n)
+		for(int j=0;j<n;j++) {          //O(n)
+			int resp = 0;               //O(1)
+			for(int k=0;k<n;k++)            //O(n)    
+				resp += a[i][k]*b[k][j];    //op.basica => esta no loop mais interno
+			c[i][j] = resp;                 
+		}
+}
+
+//--------------------------  ///func6_slide_2
+//O(n)
+int find(int v[],int n,int elem) {
+	for(int i=0;i<n;i++)        //O(n)
+		if (v[i] == elem)       //op.basica comparacao
+			return i;
+	return -1; 
+}
+
+//O(m * n) = O(mn)
+int f(int n, int m) {
+         int v[n] = {2, 4 };
+         int consultas[m] = {2, 4};
+	for(int i=0;i<m;i++)                //O(m)
+        cout << find(v,n, consultas[i]);    //saida tem complexidade O(1), find = 0(n)
+}
+//-------------------------- // fim func6_slide_2
+
+//-------------------------- // fun1_slide3
+ //O(n)
+int busca(int v[],int n, int k) {
+	for(int i=0;i<n;i++)           
+		if(v[i]==k) return i;
+	return -1;
+}
+
+//O(n^2)
+int funcao2(int v[],int n) {       
+	int ct = 0;
+	for(int i=0;i<n;i++)            //O(n)
+		for(int j=0;j<n;j++)        //O(n)
+			ct+=v[j];               //O(1)
+	return ct;
+}
+
+// O(n) + O(n^2) + O(m)= O(n^2 + m)
+int f() {
+	int v[] = {2,3,4,5}, n, k, m;
+	busca(v,n,k);       //O(n)
+	funcao2(v,n);       //O(n^2)
+    busca(v,m,k);      //O(m)  
+}
+//--------------------------
+
+//-------------------------- // fun2_slide3
+int busca2(int v[],int n, int k) {   //O(n)
+	for(int i=0;i<n;i++)           
+		if(v[i]==k) return i;
+	return -1;
+}
+
+ //O(m)
+int f2() {
+	int v[] = {2,3,4,5}, n, k, m;
+	cin >> m;
+	busca(v,m,k);   
+}
+//--------------------------
+
+//--------------------------    // fun3_slide3
+int busca3(int v[],int n, int k) {   //O(n)
+	for(int i=0;i<n;i++) 
+		if(v[i]==k) return i;
+	return -1;
+}
+
+//O(n *m^2)
+int f3() {
+    int ans;                
+    int v[] = {2,3,4,5}, n, k, m;
+	for(int i=0;i<n;i++)     //O(n)
+		for(int j=0;j<m;j++)     //O(m)
+			ans += busca(v,m,k);    //O(m) //op.basica
+}
+//--------------------------    // fun4_slide3
+int busca4(int v[],int n, int k) {
+	for(int i=0;i<n;i++) 
+		if(v[i]==k) return i;
+	return -1;
+}
+
+int funcao3(int v[],int n) {
+    
+	int ct = 0;
+	for(int i=0;i<n;i++)
+		for(int j=0;j<n;j++)
+			ct+=v[j];
+	return ct;
+}
+
+int f4() {
+    int v[] = {2,3,4,5}, n, k, m;
+    int ans=0;
+	for(int i=0;i<n;i++) {
+		for(int j=0;j<m;j++)
+			ans += busca(v,m,k);
+	funcao2(v,n);
+} 
+//--------------------------
 }
 
 int main(){
