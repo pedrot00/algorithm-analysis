9d77e95f09f37a34aaf2fee2905b822edaadf55b
Pedro S. Teixeira <pedrotex00santos@gmail.com>
2025-04-26 11:11:58 -0300

    termina MyList

--- Algoritmos/2º Unidade/MyListStruct.h
+++ Algoritmos/2º Unidade/MyListStruct.h
@@ -4,6 +4,7 @@ template <class T>
 struct Node{
     T data;
     Node *next;
+    Node(T val) : data(val), next(nullptr) {} //construtor ja com o valor data
 };
 
 template <class T>
@@ -14,74 +15,42 @@ private:
     int dataSize = 0;
 
 public:
-    /*ok */ MyList(T val);       //construtor com valor inicial
-    /*ok */ MyList();             //construtor padrao
-    /*ok */ ~MyList(){clear();}
-    /*ok */ void clear();
-    
-    insert();     
-    push_back(); 
-    front_back();  
-    /*ok */ void pop_back();   
+    /*ok */ MyList(T val);                           //construtor com valor inicial
+    /*ok */ MyList();                                //construtor padrao
+    /*ok */ ~MyList(){clear();}                      //destrutor
+    /*ok */ void clear();                            // limpa a lista
+        
+    /*ok */ void push_back(T val);                   // insere elemento no final
+    /*ok */ void push_front(T val);                  // insere elemento no início
+    /*ok */ void pop_back();                         // remove elemento do final
+    /*ok */ void insert(T val, int pos);             // insere elemento em posição específica
 
-    /*ok */ void imprimir();
-    /*ok */ void imprimirRecursivo(Node<T>* ptr);
-    /*ok */ void imprimirRecursivo();
-    /*ok */ void imprimirInverso(Node<T> *ptr);
-    /*ok */ void imprimirInverso();
+    /*ok */ void imprimir();                         // imprime lista iterativamente
+    /*ok */ void imprimirRecursivo(Node<T>* ptr);    // imprime lista recursivamente (ajudante)
+    /*ok */ void imprimirRecursivo();                // imprime lista recursivamente (público)
+    /*ok */ void imprimirInverso(Node<T> *ptr);      // imprime inversamente (ajudante)
+    /*ok */ void imprimirInverso();                  // imprime inversamente (público)
 
-    
-    /*ok */ int size() const; 
-    /*ok */ bool empty();
-    /*ok */ void copy(Node<T>* other);
-    /*ok */ Node<T>* find(Node<T>*ptr, T val);
-    order();    //ordena com o merge
-    
+    // ---------------- Funçoes importantes ----------------
+    /*ok */ int size() const;                        // retorna o tamanho da lista
+    /*ok */ bool empty();                            // verifica se a lista está vazia
+    /*ok */ void copy(Node<T>* other);               // copia outra lista
+    /*ok */ Node<T>* find(Node<T>*ptr, T val);       // busca valor recursivamente(ajudante)
+    /*ok */ Node<T>* buscar(T val);                  // busca valor recursivamente(publico)
 };
 
-//Funcoes importantes -------------
+
+// ---------------- Construtores e destrutor ----------------
 template<class T>
-int MyList<T>::size() const{
-    return dataSize;
-}
-
-template <class T>
-bool MyList<T>::empty(){
-  return dataSize == 0;
-}
-
-template<class T>
-void MyList<T>::copy(Node<T>* other){
-    clear();
-    
-    while(other){
-        push_back(other->data);
-        other = other->next;
-    }
-}
-
-template <class T>
-Node<T>* MyList<T>::find(Node<T>*ptr, T elem){
-    if (!ptr) return nullptr;
-    
-    if(ptr->data == elem) return ptr;
-    return find(ptr->next, elem);
-}
-//---------------------------------
-
-//Construtores e destrutores -------------
-template <class T>
-MyList<T>::MyList(){
-    Node<T>* ptr = new Node<T>;
-    ptr->data = T();
-    ptr->next = nullptr;
-    head = ptr;
-    tail = ptr;
+MyList<T>::MyList() {
+    head = nullptr;
+    tail = nullptr;
+    dataSize = 0;
 }
 
 template <class T>
 MyList<T>::MyList(T val){
-    Node<T>*ptr = new Node<T>;      //isso aqui eh a criacao padrao
+    Node<T>*ptr = new Node<T>(val);      //isso aqui eh a criacao padrao
     ptr->data = val;
     ptr->next = nullptr;
     head = ptr;
@@ -102,9 +71,84 @@ void MyList<T>::clear(){
     dataSize = 0;
 }
 
-//----------------------------------------
+// ---------------- Inserir e Remover ----------------
+template <class T>
+void MyList<T>::push_back(T val){
+    Node<T>* newNode = new Node<T>(val);
+    newNode->next = nullptr;
 
-// Saida e impressoes -------------------
+    if(empty()){
+        head = newNode;
+        tail= newNode;
+    }
+    else{
+       tail->next = newNode;
+       tail = newNode; 
+    }
+    dataSize++;
+}
+
+template<class T>
+void MyList<T>::push_front(T val){
+    Node<T>*newNode = new Node<T>(val);
+    
+    if(empty()){
+        head = newNode;
+        tail = newNode;
+    }
+    else{   
+        newNode->next = head;
+        head = newNode;
+    }
+    dataSize++;
+}
+
+template <class T>
+void MyList<T>::pop_back(){
+    if(empty()) return;
+    if (head == tail){
+        delete head;
+        head = nullptr;
+        tail = nullptr;
+    }
+    else{
+        Node<T> *current = head;
+        while(current->next != tail){
+            current = current->next;
+        }
+        delete tail;
+        tail = current;
+        tail->next = nullptr;
+    }
+    dataSize--;
+}
+
+template<class T>
+void MyList<T>::insert(T val, int pos){
+    if(pos < 0 || pos > dataSize){
+        std::cout << "Posicao invalida." << std::endl; 
+        return;
+    }
+    else if(pos == 0){
+        push_front(val);
+    }
+    else if(pos == dataSize){
+        push_back(val);
+    }
+    else{
+        Node<T>* newNode = new Node<T>(val);
+        Node<T>* aux = head;
+
+        for(int i = 0; i < pos; i++){
+            aux = aux->next;
+        }
+        newNode->next = aux->next;
+        aux = newNode;
+        dataSize++;
+    }
+}
+
+//  ---------------- Impressões ----------------
 template<class T>
 void MyList<T>::imprimir(){
     if(dataSize == 0)return;
@@ -137,31 +181,42 @@ void MyList<T>::imprimirInverso(Node<T>*ptr){
     std::cout<< ptr->data << " ";
 }
 
-template <class T>
+template <class T>  
 void MyList<T>::imprimirInverso(){
     imprimirInverso(head);
 }
 
-//----------------------------------------
-
-// inserir e remover ---------------------
-template <class T>
-void MyList<T>::pop_back(){
-    if(empty()) return;
-    if (head == tail){
-        delete head;
-        head = nullptr;
-        tail = nullptr;
-    }
-    else{
-        Node<T> *current = head;
-        while(current->next != tail){
-            current = current->next;
-        }
-        delete tail;
-        tail = current;
-        tail->next = nullptr;
-    }
-    dataSize--;
+// ---------------- Funções importantes ----------------
+template<class T>
+int MyList<T>::size() const{
+    return dataSize;
 }
-//----------------------------------------
+
+template <class T>
+bool MyList<T>::empty(){
+  return dataSize == 0;
+}
+
+template<class T>
+void MyList<T>::copy(Node<T>* other){
+    clear();
+    if (!other) return;
+
+    while(other){
+        push_back(other->data);
+        other = other->next;
+    }
+}
+
+template <class T>
+Node<T>* MyList<T>::find(Node<T>*ptr, T elem){
+    if (!ptr) return nullptr;
+
+    if(ptr->data == elem) return ptr;
+    return find(ptr->next, elem);
+}
+
+template <class T>
+Node<T>* MyList<T>::buscar(T val) {
+    return find(head, val);
+}
--- Algoritmos/2º Unidade/testeMyList.cpp
+++ Algoritmos/2º Unidade/testeMyList.cpp
@@ -1,8 +1,176 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 #include "MyListStruct.h"
-using namespace std;
 
+void testarInicio() {
+    std::cout << "\n===== TESTE DOS CONSTRUTORES =====\n";
+    
+    std::cout << "Criando lista com construtor padrao: ";
+    MyList<int> lista1;
+    std::cout << "Tamanho: " << lista1.size() << ", Conteudo: ";
+    lista1.imprimir();
+    
+    std::cout << "Criando lista com valor inicial 42: ";
+    MyList<int> lista2(42);
+    std::cout << "Tamanho: " << lista2.size() << ", Conteudo: ";
+    lista2.imprimir();
+}
 
-int main(){
-    MyList<int> v(10);
+void testarInsercao() {
+    std::cout << "\n===== TESTE DE INSERCAO =====\n";
+    
+    MyList<int> lista;
+    std::cout << "Lista vazia criada. Tamanho: " << lista.size() << std::endl;
+    
+    std::cout << "Inserindo elementos com push_back: ";
+    for (int i = 1; i <= 5; i++) {
+        lista.push_back(i * 10);
+    }
+    std::cout << "Tamanho: " << lista.size() << ", Conteudo: ";
+    lista.imprimir();
+    
+    std::cout << "Inserindo elementos com push_front: ";
+    for (int i = 1; i <= 3; i++) {
+        lista.push_front(i);
+    }
+    std::cout << "Tamanho: " << lista.size() << ", Conteudo: ";
+    lista.imprimir();
+    
+    std::cout << "Inserindo valor 999 na posicao 4: ";
+    lista.insert(999, 4);
+    std::cout << "Tamanho: " << lista.size() << ", Conteudo: ";
+    lista.imprimir();
+    
+    std::cout << "Inserindo valor 888 na posicao 0: ";
+    lista.insert(888, 0);
+    std::cout << "Tamanho: " << lista.size() << ", Conteudo: ";
+    lista.imprimir();
+    
+    std::cout << "Inserindo valor 777 na ultima posicao: ";
+    lista.insert(777, lista.size());
+    std::cout << "Tamanho: " << lista.size() << ", Conteudo: ";
+    lista.imprimir();
+    
+    std::cout << "Tentando inserir em posicao invalida (-1): ";
+    lista.insert(123, -1);
+    
+    std::cout << "Tentando inserir em posicao invalida (tamanho+1): ";
+    lista.insert(123, lista.size() + 1);
+}
+
+void testarRemocao() {
+    std::cout << "\n===== TESTE DE REMOCAO =====\n";
+    
+    MyList<int> lista;
+    for (int i = 1; i <= 5; i++) {
+        lista.push_back(i);
+    }
+    
+    std::cout << "Lista inicial: ";
+    lista.imprimir();
+    
+    std::cout << "Removendo elemento do final: ";
+    lista.pop_back();
+    std::cout << "Conteudo: ";
+    lista.imprimir();
+    
+    std::cout << "Removendo outro elemento do final: ";
+    lista.pop_back();
+    std::cout << "Conteudo: ";
+    lista.imprimir();
+    
+    std::cout << "Removendo todos os elementos restantes: ";
+    while (!lista.empty()) {
+        lista.pop_back();
+    }
+    std::cout << "Tamanho: " << lista.size() << ", Conteudo: ";
+    lista.imprimir();
+    
+    std::cout << "Tentando remover de lista vazia: ";
+    lista.pop_back();
+    std::cout << "Tamanho: " << lista.size() << std::endl;
+}
+
+void testarImpressao() {
+    std::cout << "\n===== TESTE DE IMPRESSAO =====\n";
+    
+    MyList<char> lista;
+    std::string palavra = "ALGORITMOS";
+    
+    for (char c : palavra) {
+        lista.push_back(c);
+    }
+    
+    std::cout << "Lista normal: ";
+    lista.imprimir();
+    
+    std::cout << "Lista recursiva: ";
+    lista.imprimirRecursivo();
+    std::cout << std::endl;
+    
+    std::cout << "Lista inversa: ";
+    lista.imprimirInverso();
+    std::cout << std::endl;
+}
+
+void testarBusca() {
+    std::cout << "\n===== TESTE DE BUSCA =====\n";
+        
+    MyList<int> lista;
+    for (int i = 10; i <= 50; i += 10) {
+        lista.push_back(i);
+    }
+        
+    std::cout << "Lista: ";
+    lista.imprimir();
+        
+    int valoresBusca[] = {20, 40, 99};
+    for (int val : valoresBusca) {
+        Node<int>* resultado = lista.buscar(val);  // Usando a nova função
+        std::cout << "Buscando valor " << val << ": ";
+        if (resultado) {
+            std::cout << "Encontrado (" << resultado->data << ")" << std::endl;
+        } else {
+            std::cout << "Nao encontrado" << std::endl;
+        }
+    }
+}
+
+void testarCopy() {
+    std::cout << "\n===== TESTE DE COPIA =====\n";
+    
+    MyList<int> listaOriginal;
+    for (int i = 1; i <= 5; i++) {
+        listaOriginal.push_back(i);
+    }
+    
+    std::cout << "Lista original: ";
+    listaOriginal.imprimir();
+    
+    MyList<int> listaCopia;
+    std::cout << "Lista copia antes: ";
+    listaCopia.imprimir();
+    
+    // NOTA: Este teste nao funciona assim diretamente,
+    // pois copy recebe um Node*, nao uma lista inteira
+    // Este eh apenas um exemplo conceitual!!!
+    std::cout << "Apos copia (conceitual - precisa ajuste): " << std::endl;
+    std::cout << "Para testar a funcao copy corretamente, ela precisa ser modificada" << std::endl;
+    std::cout << "ou voce precisa acessar o head da lista original diretamente." << std::endl;
+}
+
+int main() {
+    std::cout << "TESTES DA CLASSE MyList<T>\n";
+    std::cout << "=========================\n";
+    
+    testarInicio();
+    testarInsercao();
+    testarRemocao();
+    testarImpressao();
+    testarBusca();
+    testarCopy();
+    
+    std::cout << "\nTodos os testes foram executados.\n";
+    
+    return 0;
 }
